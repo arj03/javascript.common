b3ae2dd8b23cb6b16acc801c240da174d75cb016
Anders Rune Jensen <anders@iola.dk>
2012-06-20 15:27:04 +0200

    Initial commit

---
+++ common.js
@@ -0,0 +1,52 @@
+// similar to format in other languages
+function format(str) {
+    for (var i = 1; i < arguments.length; ++i)
+        str = str.replace(new RegExp('\\{' + (i - 1) + '\\}', "g"), arguments[i]);
+
+    return str;
+}
+
+// deep clone
+function cloneObj(o) {
+    return jQuery.extend(true, {}, o);
+}
+
+// IE doesn't have trim
+if (typeof String.prototype.trim !== 'function') {
+  String.prototype.trim = jQuery.trim;
+}
+
+// this will also escape the name
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+    var results = regex.exec(window.location.href);
+    if (results == null)
+        return "";
+    else
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function callback(func, json, suc, fail) {
+    $.ajax({
+        type: "POST",
+        url: window.location.pathname + "/" + func,
+        contentType: "application/json; charset=utf-8",
+        data: json,
+        dataType: "json",
+        success: suc,
+        error: fail
+    });
+}
+
+function callbackUrl(url, func, json, suc, fail) {
+    $.ajax({
+        type: "POST",
+        url: url + "/" + func,
+        contentType: "application/json; charset=utf-8",
+        data: json,
+        dataType: "json",
+        success: suc,
+        error: fail
+    });
+}
